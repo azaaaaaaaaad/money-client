c02c3f9a31ee35d8a9e6f58d36979137a4139392
jrazad10 <jrazad10@gmail.com>
2024-07-16 12:16:13 +0600

    login and registration page created

---
+++ src/Components/Shared/Registration.jsx
@@ -0,0 +1,52 @@
+
+
+const Registration = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Registration now!</h1>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="full name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Mobile Number</span>
+                            </label>
+                            <input type="number" placeholder="mobile number" className="input input-bordered" required />
+                            {/* <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label> */}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">PIN</span>
+                            </label>
+                            <input type="number" placeholder="pin" className="input input-bordered" required />
+                            {/* <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label> */}
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Registration;
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,28 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Components/Root/Root';
+import ErrorPage from './Components/Shared/ErrorPage';
+import Login from './Components/Shared/Login';
+import Registration from './Components/Shared/Registration';
 
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/registration',
+        element: <Registration></Registration>
+      },
+    ]
   },
 ]);
 
